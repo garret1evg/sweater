af1896ed342476efe72cc259b1db5af3e471e89a
Evgenii Chmutov <garret.evg@gmail.com>
2020-02-29 23:22:18 +0200

    mustache added

--- src/main/java/ua/training/chmutov/sweater/GreetingController.java
+++ src/main/java/ua/training/chmutov/sweater/GreetingController.java
@@ -1,9 +1,10 @@
 package ua.training.chmutov.sweater;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class GreetingController {
@@ -11,9 +12,9 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Model model
+            Map<String,Object> model
     ) {
-        model.addAttribute("name", name);
+        model.put("name", name);
         return "greeting";
     }
 
